bb71ef7ce45abbbc6df1d0e9ebc55386ce387682
taoge407 <58383210+taoge407@users.noreply.github.com>
2021-12-09 17:06:09 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ let lightStatus = 0
 let ms = 0
 basic.forever(function () {
     if (lightStatus == 0) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
     } else {
-        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
     }
 })
 basic.forever(function () {
