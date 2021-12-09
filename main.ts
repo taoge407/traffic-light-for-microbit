39d6c630420601ac89106057f5bfe3d11d3323e4
taoge407 <58383210+taoge407@users.noreply.github.com>
2021-12-09 16:57:15 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+let lightStatus = 0
 basic.forever(function () {
-	
+    lightStatus = 0
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(5000)
+    lightStatus = 1
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(30000)
+})
+basic.forever(function () {
+    if (lightStatus == 1) {
+        basic.showString("Run!")
+        for (let index = 0; index < 4; index++) {
+            music.playTone(262, music.beat(BeatFraction.Eighth))
+            basic.pause(100)
+        }
+        music.playTone(247, music.beat(BeatFraction.Eighth))
+    } else {
+        basic.showString("Stop")
+        music.playTone(220, music.beat(BeatFraction.Quarter))
+        basic.pause(500)
+    }
 })
